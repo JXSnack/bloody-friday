0131a0ab7c6e544dc69affe9721334a7b1202711
JXSnack <jx.snack@gmail.com>
2026-04-06 00:02:49 +0200

    handle window resize

--- client/src/scene/world.ts
+++ client/src/scene/world.ts
@@ -2,6 +2,7 @@ import { Scene3D } from "enable3d";
 import {Entity} from "../entity/entity";
 import {Player} from "../entity/player"
 import {Game} from "../util";
+import {PerspectiveCamera} from "three";
 
 export class MainScene extends Scene3D {
     private entities: Entity[] = [];
@@ -19,6 +20,14 @@ export class MainScene extends Scene3D {
         this.addEntity(Game.self);
 
         this.camera.position.set(0, 10, 15);
+
+        window.onresize = () => {
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+            const cam = this.camera as PerspectiveCamera;
+            cam.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        }
     }
 
     update() {
