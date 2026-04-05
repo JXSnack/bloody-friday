05a0ef2afb2ad161fd9f3344e74a97022ea5f4c2
JXSnack <jx.snack@gmail.com>
2026-04-05 23:58:59 +0200

    movement controller

--- client/src/entity/player.ts
+++ client/src/entity/player.ts
@@ -1,6 +1,6 @@
 import {Entity} from "./entity";
 import {Scene3D} from "enable3d";
-import {Game} from "../util";
+import {Game, Vec} from "../util";
 
 export class Player extends Entity {
     constructor(scene: Scene3D) {
@@ -9,25 +9,56 @@ export class Player extends Entity {
 
     create() {
         this.mesh = this.scene.physics.add.box(
-            {...this.pos, ...this.hitboxSize, mass: this.mass},
+            {...new Vec(0, 2, 0), ...this.hitboxSize, mass: this.mass},
             {phong: {color: 0xffffff}}
         );
     }
 
     update() {
-        const speed = 5;
-        let vx= 0, vz = 0;
+        if (this.mesh == null) return;
+        if (Game.self === this) this.handlePlayerControls();
 
-        if (Game.keys["KeyA"]) vx = -speed;
-        if (Game.keys["KeyD"]) vx =  speed;
-        if (Game.keys["KeyW"]) vz = -speed;
-        if (Game.keys["KeyS"]) vz =  speed;
+        super.update();
+    }
 
-        const vy = this.mesh.body.velocity.y;
-        this.mesh.body.setVelocity(vx, vy, vz);
+    private handlePlayerControls() {
+        const maxSpeed = 5;
+        const accel = 0.2;     // how fast you speed up
+        const friction = 0.15; // how fast you slow down
 
-        if (Game.keys["Space"]) {
-            this.mesh.body.applyForceY(0.5)
+        let inputX = 0;
+        let inputZ = 0;
+
+        if (Game.keys["KeyA"]) inputX -= 1;
+        if (Game.keys["KeyD"]) inputX += 1;
+        if (Game.keys["KeyW"]) inputZ -= 1;
+        if (Game.keys["KeyS"]) inputZ += 1;
+
+        // normalize input
+        const len = Math.hypot(inputX, inputZ);
+        if (len > 0) {
+            inputX /= len;
+            inputZ /= len;
+        }
+
+        const targetVX = inputX * maxSpeed;
+        const targetVZ = inputZ * maxSpeed;
+
+        const current = this.mesh.body.velocity;
+
+        // interpolate toward target velocity
+        const vx = current.x + (targetVX - current.x) * accel;
+        const vz = current.z + (targetVZ - current.z) * accel;
+
+        // apply friction when no input
+        const finalVX = len === 0 ? current.x * (1 - friction) : vx;
+        const finalVZ = len === 0 ? current.z * (1 - friction) : vz;
+
+        this.vel = new Vec(finalVX, 0, finalVZ);
+
+        // jump
+        if (Game.keys["Space"] && this.isColliding()) {
+            this.mesh.body.setVelocityY(6);
         }
     }
 }
