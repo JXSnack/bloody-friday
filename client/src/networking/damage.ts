a66a6a8937c8e8674cf3560b74db69ddb25337d4
JXSnack <jx.snack@gmail.com>
2026-04-08 20:52:40 +0200

    send shooting stuff

--- client/src/networking/damage.ts
+++ client/src/networking/damage.ts
@@ -1,4 +1,4 @@
-import {debug, Game} from "../util";
+import {debug, Game, Vec} from "../util";
 import {Player} from "../entity/player";
 import {KillOverlay} from "../hud/killOverlay";
 
@@ -12,4 +12,21 @@ export function handleKill(sender: string, data: any) {
     let entity: Player = Game.world?.getEntity(sender) as Player;
     debug(entity);
     KillOverlay.INSTANCE.doKilled(entity.name);
+}
+
+export function handleSomeShot(sender: string, data: any) {
+    let pos: Vec = data["pos"];
+    const selfPos = Game.self!.getPos();
+
+    const dx = Math.abs(pos.x - selfPos.x);
+    const dy = Math.abs(pos.y - selfPos.y);
+    const dz = Math.abs(pos.z - selfPos.z);
+
+    if (dx > 20 || dy > 20 || dz > 20) return; // Inaudible, skip
+
+    // 1.0 at center, quieter toward the edges
+    const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+    const volume = Math.max(0, 1 - dist / (5 * Math.sqrt(3))) * 0.5; // max 0.5, quieter the farther away
+
+    Game.sounds.playMiss(volume);
 }
