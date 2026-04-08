0be9c19ac0a2b2b91a20f3ebdd1c6907f5c246aa
JXSnack <jx.snack@gmail.com>
2026-04-08 19:31:49 +0200

    prod/dev

---
+++ client/webpack.prod.cjs
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.ts",
+    mode: "development",
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
+        }],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./index.html" }),
+    ],
+    devServer: {
+        host: '127.0.0.1',
+        allowedHosts: ['friday.snackbag.net', '127.0.0.1'],
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        port: 5173,
+        hot: false,
+        liveReload: false,
+        client: false,
+        webSocketServer: false,
+    },
+};
